ca728958fa0392318ba8a2d8f4f4edd737f27591
KateSpillane <kate.m.spillane.24@dartmouth.edu>
2024-05-22 13:24:09 -0400

    started main

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include<stdio.h> 
 #include<stdlib.h> 
+#include "y.tab.h"
+#include<vector>
+#include<string>
+using namespace std;
+extern int yyparse();
+extern int yylex_destroy();
+extern FILE *yyin;
+extern vector<string> tokens;
 
-int main(){
-    
+void processFile(char* filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "Unable to open file");
+        exit(0);
+    }
+    yyin = file;
+    yyparse();
+    fclose(file);
+}
+
+int main(int argc, char* argv[]){
+    if(argv != 3){
+        fprintf(stderr, "Usage: %s prog1.c prog2.c\n", argv[1]);
+        exit(1);
+    }
+    processFile(argv[1]);
 }
